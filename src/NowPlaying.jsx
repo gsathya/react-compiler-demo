f0317daaa9497c8682829f5e76b7a68f6f0b4887
Sathya Gunsasekaran <gsathya.ceg@gmail.com>
2024-05-08 16:34:40 +0100

    update to react beta and simplify vite config

--- src/NowPlaying.jsx
+++ src/NowPlaying.jsx
@@ -1,4 +1,4 @@
-import { Slider } from "@/components/ui/slider";
+import { Slider } from "./components/ui/slider";
 import { PlayerControls } from "./PlayerControls";
 
 export function NowPlaying({ song, onChange }) {
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
@@ -6,28 +5,8 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        // plugins: [
-        //   ["babel-plugin-react-forget", { enableUseMemoCachePolyfill: true }],
-        // ],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: path.resolve(__dirname),
-      },
-      {
-        find: "react-forget-runtime",
-        replacement: path.resolve(
-          __dirname,
-          "./node_modules/react-forget-runtime",
-        ),
-      },
-    ],
-  },
-  build: {
-    minify: "false",
-  },
 });
